5ef4d3ad7860ad1a6ce0d81c495eb840baaff400
Ricardo Cerqueira <github@cerqueira.org>
2011-11-25 15:30:36 +0000

    build: Preliminary support for Turbo product builds
    If building a cm_* product, skip crawling the filesystem for
    AndroidProduct makefiles (and, of course, parsing their contents)
    and aim directly for the device/cm/<product> device configuration
    
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    
    Change how we search for CM makefiles.
    
    build: Fix fastpath code for CM_BUILD
    
     * We need to adjust to the new validations
    
    build: Modify for Turbo
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -68,6 +68,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^turbo_") ; then
+       TURBO_BUILD=$(echo -n $1 | sed -e 's/^turbo_//g')
+    else
+       TURBO_BUILD=
+    fi
+    export TURBO_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
