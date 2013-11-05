789dd18fb79a1ddcd9869f1bd3e4c852d196a71f
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-05 22:17:59 +0000

    build: Fix global selinux policy inclusion
    We're adding CM-specific policies to an overlay instead of patching
    external/sepolicy, so we need to make sure it's included to prevent
    misbehaving systems
    
    Change-Id: I7989a1093f9abc8360d492e73a860eb4afad10ad
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -690,4 +690,8 @@ RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting from device configs
+$(eval include vendor/turbo/sepolicy/sepolicy.mk)
+
 include $(BUILD_SYSTEM)/dumpvar.mk
