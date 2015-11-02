e96b870f966aede016684c692ff39f8277fcfb63
Michael Bestas <mikeioannina@gmail.com>
2015-11-02 02:10:26 +0200

    full_base: Fix mismerge
    * Remove property that was mismerged in https://github.com/TurboROM/build/commit/76317322433c1bd7ead2c448cef9cc2ce68d68cc
    
    Change-Id: I18be52cc4a167f52150e56859f5139bda09d34e4
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -30,10 +30,6 @@ PRODUCT_PACKAGES += \
     PhaseBeam \
     PhotoTable
 
-# Additional settings used in all AOSP builds
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.com.android.dateformat=MM-dd-yyyy
-
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
