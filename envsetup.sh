LineageOS/android_build

envsetup: push to connected device only if it is $CM_BUILD
Change-Id: Icce057cb31d6fbc9abb0292311c88c32fea3540c

envsetup.sh
@@ -673,6 +673,8 @@ function eat()
             done
             echo "Device Found.."
         fi
+    if (adb shell cat /system/build.prop | grep -q "ro.cm.device=$CM_BUILD");
+    then
         # if adbd isn't root we can't write to /cache/recovery/
         adb root
         sleep 1
@@ -695,6 +697,9 @@ EOF
         return 1
     fi
     return $?
+    else
+        echo "The connected device does not appear to be $CM_BUILD, run away!"
+    fi
 }
 
 function omnom
@@ -1755,6 +1760,8 @@ function dopush()
         echo "Device Found."
     fi
 
+    if (adb shell cat /system/build.prop | grep -q "ro.cm.device=$CM_BUILD");
+    then
     adb root &> /dev/null
     sleep 0.3
     adb wait-for-device &> /dev/null
@@ -1793,6 +1800,9 @@ function dopush()
     done
     rm -f $OUT/.log
     return 0
+    else
+        echo "The connected device does not appear to be $CM_BUILD, run away!"
+    fi
 }
 
 alias mmp='dopush mm'

TurboROM-Legacy/build

Conflicts:
	envsetup.sh

envsetup.sh
@@ -749,6 +749,8 @@ function eat()
             done
             echo "Device Found.."
         fi
+    if (adb shell cat /system/build.prop | grep -q "ro.slim.device=$SLIM_BUILD");
+    then
         # if adbd isn't root we can't write to /cache/recovery/
         adb root
         sleep 1
@@ -768,6 +770,9 @@ EOF
         return 1
     fi
     return $?
+    else
+        echo "The connected device does not appear to be $SLIM_BUILD, run away!"
+    fi
 }
 
 function gettop
@@ -2184,6 +2189,8 @@ function dopush()
         echo "Device Found."
     fi
 
+    if (adb shell cat /system/build.prop | grep -q "ro.slim.device=$SLIM_BUILD");
+    then
     # retrieve IP and PORT info if we're using a TCP connection
     TCPIPPORT=$(adb devices | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+[^0-9]+' \
         | head -1 | awk '{print $1}')
@@ -2245,6 +2252,9 @@ function dopush()
     fi
     rm -f $OUT/.log
     return 0
+    else
+        echo "The connected device does not appear to be $SLIM_BUILD, run away!"
+    fi
 }
 
 alias mmp='dopush mm'