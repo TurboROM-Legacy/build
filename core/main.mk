58ca4a359266860057c3b813cd7fe25ab68f7c55
Raleigh Matlock <raleighman2@gmail.com>
2016-02-17 12:36:35 -0500

    Ensure the removal of old changelog

--- core/main.mk
+++ core/main.mk
@@ -1116,7 +1116,8 @@ dirty:
 	@rm -rf $(OUT_DIR)/target/product/*/system/build.prop
 	@rm -rf $(OUT_DIR)/target/product/*/*.zip
 	@rm -rf $(OUT_DIR)/target/product/*/*.md5sum
-	@echo -e ${CL_GRN}"build.prop and zip files erased"${CL_RST}
+	@rm -rf $(OUT_DIR)/target/product/*/Changelog.txt
+	@echo -e ${CL_GRN}"build.prop, changelog, and zip files erased"${CL_RST}
 
 #xxx scrape this from ALL_MODULE_NAME_TAGS
 .PHONY: modules
