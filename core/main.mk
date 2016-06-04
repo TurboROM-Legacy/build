7a5a1fa79f97d1e2d45658e7a254b348e81ec5dd
Raleigh Matlock <raleighman2@gmail.com>
2016-06-03 20:18:49 -0400

    [SQUASH] Fix building with newer JDKs warn and continue if using wrong jdk Author: jumoog <kilianvonpflugk@gmail.com>
    JDK8: JDK check should succeed with Java 8
    Author: mar-v-in <github@rvin.mooo.com>
    
    Fix for Java 8 toolchain builds
    Author: Neil Fuller <nfuller@google.com>
    
    Change-Id: I1c1eaff7fa36bba2db261e0c5311bbc6d005a726
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -148,13 +148,13 @@ javac_version_str := $(shell unset _JAVA_OPTIONS && javac -version 2>&1)
 ifneq ($(EXPERIMENTAL_USE_JAVA8),)
 required_version := "1.8.x"
 required_javac_version := "1.8"
-java_version := $(shell echo '$(java_version_str)' | grep 'openjdk .*[ "]1\.8[\. "$$]')
+java_version := $(shell echo '$(java_version_str)' | grep '[ "]1\.8[\. "$$]')
 javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.8[\. "$$]')
 else # default
-required_version := "1.7.x"
-required_javac_version := "1.7"
-java_version := $(shell echo '$(java_version_str)' | grep '^java .*[ "]1\.7[\. "$$]')
-javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.7[\. "$$]')
+required_version := "1.7.x/1.8.x"
+required_javac_version := "1.7/1.8"
+java_version := $(shell echo '$(java_version_str)' | grep -E '^(java|openjdk) .*[ "]1\.[78][\. "$$]')
+javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.[78][\. "$$]')
 endif # if EXPERIMENTAL_USE_JAVA8
 
 ifeq ($(strip $(java_version)),)
@@ -189,7 +189,7 @@ $(info ************************************************************)
 $(info You asked for an OpenJDK 7 build but your version is)
 $(info $(java_version_str).)
 $(info ************************************************************)
-$(error stop)
+$(info You may continue but you are warned!)
 endif # java version is not OpenJdk
 else # if requires_openjdk
 ifneq ($(shell echo '$(java_version_str)' | grep -i openjdk),)
@@ -200,7 +200,7 @@ $(info You use OpenJDK but only Sun/Oracle JDK is supported.)
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
+$(info You may continue but you are warned!)
 endif # java version is not Sun Oracle JDK
 endif # if requires_openjdk
 
@@ -216,7 +216,7 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
+$(info You may continue but you are warned!)
 endif
 
 
