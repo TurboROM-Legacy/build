decae062d5f49c4d25e6a9c3d7a0e562900aa66f
Raleigh Matlock <raleighman2@gmail.com>
2016-08-19 08:03:45 -0400

    [SQUASH] Revert all protobuf changes
    Revert "Revert "binary: Generate dummy rule for relocated protobuf headers""
    
    This reverts commit df793a4.
    
    Revert "binary: Generate dummy rule for relocated protobuf headers"
    
    This reverts commit 86b7669.
    
    Revert "Allow a device to generically define its own headers"
    
    This reverts commit ef61380.
    
    Revert "binary: Don't run protobuf header relocations if origin/target are the same"
    
    This reverts commit bfa4b18.
    
    Revert "binary: Allow specifying a path for generated proto headers"
    
    This reverts commit 3a5e36c.
    
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- core/binary.mk
+++ core/binary.mk
@@ -710,27 +710,12 @@ proto_generated_headers := $(patsubst %.pb$(my_proto_source_suffix),%.pb.h, $(pr
 proto_generated_objects := $(addprefix $(proto_generated_obj_dir)/, \
     $(patsubst %.proto,%.pb.o,$(proto_sources_fullpath)))
 
-define copy-proto-files
-$(if $(PRIVATE_PROTOC_OUTPUT), \
-   $(if $(call streq,$(PRIVATE_PROTOC_INPUT),$(PRIVATE_PROTOC_OUTPUT)),, \
-   $(eval proto_generated_path := $(dir $(subst $(PRIVATE_PROTOC_INPUT),$(PRIVATE_PROTOC_OUTPUT),$@)))
-   $(eval proto_target_files := $(patsubst %.pb$(PRIVATE_PROTOC_SUFFIX), %.pb.*, $@))
-   @mkdir -p $(dir $(proto_generated_path))
-   @echo "Protobuf relocation: $(proto_target_files) => $(proto_generated_path)"
-   @cp -f $(proto_target_files) $(proto_generated_path) ),)
-endef
-
-
 # Ensure the transform-proto-to-cc rule is only defined once in multilib build.
 ifndef $(my_prefix)_$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)_proto_defined
 $(proto_generated_sources): PRIVATE_PROTO_INCLUDES := $(TOP)
 $(proto_generated_sources): PRIVATE_PROTOC_FLAGS := $(LOCAL_PROTOC_FLAGS) $(my_protoc_flags)
-$(proto_generated_sources): PRIVATE_PROTOC_OUTPUT := $(LOCAL_PROTOC_OUTPUT)
-$(proto_generated_sources): PRIVATE_PROTOC_INPUT := $(LOCAL_PATH)
-$(proto_generated_sources): PRIVATE_PROTOC_SUFFIX := $(my_proto_source_suffix)
 $(proto_generated_sources): $(proto_generated_sources_dir)/%.pb$(my_proto_source_suffix): %.proto $(PROTOC)
 	$(transform-proto-to-cc)
-	$(copy-proto-files)
 
 # This is just a dummy rule to make sure gmake doesn't skip updating the dependents.
 $(proto_generated_headers): $(proto_generated_sources_dir)/%.pb.h: $(proto_generated_sources_dir)/%.pb$(my_proto_source_suffix)
@@ -1142,11 +1127,6 @@ normal_objects := \
     $(addprefix $(TOPDIR)$(LOCAL_PATH)/,$(LOCAL_PREBUILT_OBJ_FILES))
 
 all_objects := $(normal_objects) $(gen_o_objects)
-
-## Allow a device's own headers to take precedence over global ones
-ifneq ($(TARGET_SPECIFIC_HEADER_PATH),)
-my_c_includes := $(TOPDIR)$(TARGET_SPECIFIC_HEADER_PATH) $(my_c_includes)
-endif
 
 my_c_includes += $(TOPDIR)$(LOCAL_PATH) $(intermediates) $(generated_sources_dir)
 
